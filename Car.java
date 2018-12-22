cf73bf88b2c86ec7a6a7fc1bda18dc57eb3d2ef3
eliasJ <chacinelias@users.noreply.github.com>
2018-12-22 12:21:11 -0500

    added Car class

---
+++ Car.java
@@ -0,0 +1,15 @@
+class Car{
+    String make;
+    String model;
+    int year;
+
+    public Car(String make, String model, int year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void start(){
+        System.out.println(this.model + " is starting.");
+    }
+}
